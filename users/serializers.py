97f5789d2a7b6a939964d63c227dea275804ab3b
danielnevesfonseca <daniel.neves.fonseca.ti@gmail.com>
2023-12-08 21:19:36 -0300

    feat: add authentication by login and create movies app

--- users/serializers.py
+++ users/serializers.py
@@ -39,3 +39,7 @@ class UserSerializer(serializers.Serializer):
             )
         return user
 
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128)
